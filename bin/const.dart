b001ee498dbf69fbb86e5c781d7027141034e1b3
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2023-01-08 16:18:12 +0300

    const described

---
+++ bin/const.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  // const value will not change during the execution of the program.
+  const name = 'John';
+  print('Hello, $name');
+
+  // name = 'Doe'; // Error: A const variable can't be assigned a value.
+}
