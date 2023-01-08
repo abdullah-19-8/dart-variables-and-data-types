115dbcdaa415b8d86fd0b54244c41e3066d59d57
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2023-01-08 17:28:59 +0300

    data type promotion

---
+++ bin/data_types.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  // data types help dart to know what kind of data is stored in the variable.
+
+  // final int age = '20'; // invalid operation: type 'String' is not a subtype of type 'int'
+
+  // type promotion
+
+  // this is promoting age to int type.
+  final double age = 20;
+
+  // this is demoting age to double type.
+  // final int age2 = 20.0;
+
+
+}
