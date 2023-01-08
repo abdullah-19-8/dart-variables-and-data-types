076c429060180e27c7bbd9e845439c15706b400e
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2023-01-08 16:26:19 +0300

    deffirence between const and final

--- bin/const.dart
+++ bin/const.dart
@@ -4,4 +4,17 @@ void main(List<String> args) {
   print('Hello, $name');
 
   // name = 'Doe'; // Error: A const variable can't be assigned a value.
+
+  // const value can be set only once.
+  const names = ['John', 'Doe'];
+
+  // names = ['John', 'Doe', 'Smith']; // Error: A const variable can't be assigned a value.
+
+  // But the value of a const variable can't be changed.
+
+  names.add('Smith');
+
+  // compile-time error: Unhandled exception: Unsupported operation: Cannot add to an unmodifiable list
+
+  
 }
