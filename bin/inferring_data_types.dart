a1c699d4dfd83313e569c6d6e7f6d88be916ca9b
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2023-01-08 16:55:15 +0300

    inferring data types

---
+++ bin/inferring_data_types.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  // variable can be assign to const variable only without data type and with data type.
+  const int age = 20;
+  print(age);
+  const age2 = 20;
+  print(age2);
+}
