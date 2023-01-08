535dd72b0c1446195b02686217cf050856257dff
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2023-01-08 18:06:07 +0300

    various data types

---
+++ bin/various_data_types.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  const double pi = 3.14;
+  print(pi);
+  const int age = 20;
+  print(age);
+  const String name = 'John';
+  print(name);
+  const bool isTrue = true;
+  print(isTrue);
+  const List<int> numbers = [1, 2, 3];
+  print(numbers);
+  const Map<String, int> map = {'one': 1, 'two': 2};
+  print(map['one']);
+  const Set<int> set = {1, 2, 3};
+  print(set.length);
+  const dynamic value = null;
+  print(value);
+  const Symbol symbol = #symbol;
+  print(symbol);
+}
